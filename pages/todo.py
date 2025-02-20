93610f8a6166ef4738280b1e7cfb06f154ee0f6f
OODAssignment1 <m.marullo25@ncf.edu>
2025-02-20 17:55:00 -0500

    fixed error for when duplicate task is entered

--- pages/todo.py
+++ pages/todo.py
@@ -1,3 +1,4 @@
+import streamlit
 import streamlit as st
 from pages import home_page as home
 
@@ -10,17 +11,19 @@ st.subheader("To-Do List")
 # Initialize tasks in session state
 if "tasks" not in st.session_state:
     st.session_state.tasks = []
-
 with st.popover("Add task"):
     todo = st.text_input("What do you need to do? :)")
     if st.button("Add Task"):
-        if todo:  # Avoid adding empty tasks
-            st.session_state.tasks.append(todo)
-            st.rerun()  # Refresh UI
+        if todo in st.session_state.tasks:
+            st.error("You cannot enter duplicate tasks!")
+        else:
+            if todo:  # Avoid adding empty tasks
+                st.session_state.tasks.append(todo)
+                st.rerun()  # Refresh UI
 
 # Display tasks with checkboxes
 with st.container():
     for task in st.session_state.tasks:
         if st.checkbox(task, key=task):
-            st.session_state.tasks.remove(task)
+            #st.session_state.tasks.remove(task)
             st.rerun()  # Refresh UI after removing checked task
